a91ed97a4f39aa00609e739b1259e89be1db78e3
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2025-01-21 08:05:13 +0100

    STM32 GPIO driver

--- app/stm32f303x8/main.c
+++ app/stm32f303x8/main.c
@@ -1,4 +1,4 @@
-#include "vehicle.h"
+#include "gpio.h"
 
 // led is PB3
 #define LED_PIN GPIO_ODR_3
@@ -6,11 +6,25 @@
 int
 main(void)
 {
-    vehicle_init();
+    gpio_t left_front_a1  = gpio_create(GPIO_L293_LEFT_FRONT_A1);
+    gpio_t left_front_a2  = gpio_create(GPIO_L293_LEFT_FRONT_A2);
+    gpio_t right_front_a1 = gpio_create(GPIO_L293_RIGHT_FRONT_A1);
+    gpio_t right_front_a2 = gpio_create(GPIO_L293_RIGHT_FRONT_A2);
+    gpio_t left_rear_a1   = gpio_create(GPIO_L293_LEFT_REAR_A1);
+    gpio_t left_rear_a2   = gpio_create(GPIO_L293_LEFT_REAR_A2);
+    gpio_t right_rear_a1  = gpio_create(GPIO_L293_RIGHT_REAR_A1);
+    gpio_t right_rear_a2  = gpio_create(GPIO_L293_RIGHT_REAR_A2);
+    gpio_set_state(&left_front_a1, GPIO_STATE_HIGH);
+    gpio_set_state(&left_front_a2, GPIO_STATE_HIGH);
+    gpio_set_state(&right_front_a1, GPIO_STATE_HIGH);
+    gpio_set_state(&right_front_a2, GPIO_STATE_HIGH);
+    gpio_set_state(&left_rear_a1, GPIO_STATE_HIGH);
+    gpio_set_state(&left_rear_a2, GPIO_STATE_HIGH);
+    gpio_set_state(&right_rear_a1, GPIO_STATE_HIGH);
+    gpio_set_state(&right_rear_a2, GPIO_STATE_HIGH);
 
     while (1)
     {
-        vehicle_run();
     }
 
     return 0;
--- src/drivers/peripherials/gpio/gpio_stm32f303x8.c
+++ src/drivers/peripherials/gpio/gpio_stm32f303x8.c
@@ -1,40 +1,88 @@
 #include "gpio.h"
 #include "stm32f3xx.h"
+#include <string.h>
 
-static void
-gpio_init_l293_left_front_a1(void);
-
-static void
-gpio_init_l293_left_front_a2(void);
-
-
-
-void
-gpio_init_output(gpio_pin_t const pin)
+typedef struct gpio_definition
 {
-    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-    gpio_init_l293_left_front_a1();
-    gpio_init_l293_left_front_a2();
+    uint32_t      rcc_gpioxen;
+    GPIO_TypeDef *gpiox;
+    uint8_t       bit;
+} gpio_definition_t;
+
+static gpio_definition_t gpio[9] = {
+    [GPIO_L293_LEFT_FRONT_A1]   = {RCC_AHBENR_GPIOAEN, GPIOA, 0},
+    [GPIO_L293_LEFT_FRONT_A2]   = {RCC_AHBENR_GPIOAEN, GPIOA, 1},
+    [GPIO_L293_RIGHT_FRONT_A1]  = {RCC_AHBENR_GPIOAEN, GPIOA, 2},
+    [GPIO_L293_RIGHT_FRONT_A2]  = {RCC_AHBENR_GPIOAEN, GPIOA, 3},
+    [GPIO_L293_LEFT_REAR_A1]    = {RCC_AHBENR_GPIOAEN, GPIOA, 4},
+    [GPIO_L293_LEFT_REAR_A2]    = {RCC_AHBENR_GPIOAEN, GPIOA, 5},
+    [GPIO_L293_RIGHT_REAR_A1]   = {RCC_AHBENR_GPIOAEN, GPIOA, 6},
+    [GPIO_L293_RIGHT_REAR_A2]   = {RCC_AHBENR_GPIOAEN, GPIOA, 7},
+    [GPIO_DUALSHOCK2_ATTENTION] = {RCC_AHBENR_GPIOAEN, GPIOA, 12},
+};
+
+gpio_t
+gpio_create(gpio_pin_t pin)
+{
+    gpio_t gpio;
+    memset(&gpio, 0, sizeof(gpio));
+    gpio.pin = pin;
+    gpio_init(&gpio);
+
+    return gpio;
 }
 
 void
-gpio_set_output(gpio_pin_t const pin, gpio_state_t const state)
+gpio_init(gpio_t *self)
 {
-    GPIOA->ODR |= (1U << 0);
+    // init object
+    self->mode  = GPIO_MODE_OUTPUT;
+    self->state = GPIO_STATE_LOW;
+
+    // init peripheral
+    gpio_definition_t *definition = &gpio[self->pin];
+
+    // This block reads RCC->AHBENR twice, but writes only when necessarry
+    // Reading is faster than writing and this is still init phase
+    if ((RCC->AHBENR & definition->rcc_gpioxen) == 0)
+    {
+        RCC->AHBENR |= definition->rcc_gpioxen;
+    }
+
+    // Get MODER bits by pin index. There is always MODER two bits for each GPIO
+    uint32_t moderx_pos = definition->bit * 2;
+    uint32_t moderx_0   = (1 << moderx_pos);
+    uint32_t moderx_1   = (2 << moderx_pos);
+
+    if (self->mode == GPIO_MODE_OUTPUT)
+    {
+        definition->gpiox->MODER |= moderx_0;
+        definition->gpiox->MODER &= ~moderx_1;
+    }
+    else
+    {
+        definition->gpiox->MODER &= ~(moderx_0 | moderx_1);
+    }
 }
 
-static void
-gpio_init_l293_left_front_a1(void)
+void
+gpio_set_state(gpio_t *self, gpio_state_t const state)
 {
-    // set PA0 as output
-    GPIOA->MODER |= GPIO_MODER_MODER0_0;
-    GPIOA->MODER &= ~GPIO_MODER_MODER0_1;
-}
+    gpio_definition_t *definition = &gpio[self->pin];
 
-static void
-gpio_init_l293_left_front_a2(void)
-{
-    // set PA1 as output
-    GPIOA->MODER |= GPIO_MODER_MODER1_0;
-    GPIOA->MODER &= ~GPIO_MODER_MODER1_1;
+    if (state == self->state)
+    {
+        return;
+    }
+
+    if (state == GPIO_STATE_LOW)
+    {
+        definition->gpiox->ODR &= ~(1 << definition->bit);
+    }
+    else
+    {
+        definition->gpiox->ODR |= (1 << definition->bit);
+    }
+
+    self->state = state;
 }
