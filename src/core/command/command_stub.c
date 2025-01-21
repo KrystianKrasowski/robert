8e2237e4686cf5b3440af3863ae336d63bc61b08
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2025-01-22 00:17:00 +0100

    dummy command sequence

--- src/core/command/command_stub.c
+++ src/core/command/command_stub.c
@@ -3,6 +3,32 @@
 
 static volatile int i = 0;
 
+static command_t sequence[20] =
+    {
+        COMMAND_MOVE_FACE_FORWARD,
+        COMMAND_MOVE_FACE_BACKWARD,
+        COMMAND_MOVE_SIDE_RIGHT,
+        COMMAND_MOVE_SIDE_LEFT,
+        COMMAND_MOVE_DIAG_FORWARD_RIGHT,
+        COMMAND_MOVE_DIAG_FORWARD_LEFT,
+        COMMAND_MOVE_DIAG_BACKWARD_RIGHT,
+        COMMAND_MOVE_DIAG_BACKWARD_LEFT,
+        COMMAND_ROTATE_SIDE_RIGHT_FORWARD,
+        COMMAND_ROTATE_SIDE_LEFT_FORWARD,
+        COMMAND_ROTATE_SIDE_RIGHT_BACKWARD,
+        COMMAND_ROTATE_SIDE_LEFT_BACKWARD,
+        COMMAND_ROTATE_RIGHT,
+        COMMAND_ROTATE_LEFT,
+        COMMAND_ROTATE_FACE_FRONT_RIGHT,
+        COMMAND_ROTATE_FACE_FRONT_LEFT,
+        COMMAND_ROTATE_FACE_REAR_RIGHT,
+        COMMAND_ROTATE_FACE_REAR_LEFT,
+        COMMAND_SOFT_STOP,
+        COMMAND_HARD_STOP,
+};
+
+static volatile int index = 0;
+
 void
 command_init(void)
 {
@@ -12,25 +38,16 @@ command_init(void)
 command_t
 command_receive(void)
 {
-    if (i == 0)
-    {
-        return COMMAND_ROTATE_FACE_FRONT_LEFT;
-    }
-    else
-    {
-        return COMMAND_ROTATE_FACE_FRONT_RIGHT;
-    }
+    return sequence[index];
 }
 
 void
 timer_compare_a_match_isr(void)
 {
-    if (i == 0)
+    index++;
+    
+    if (index == 20)
     {
-        i = 1;
-    }
-    else
-    {
-        i = 0;
+        index = 0;
     }
 }
